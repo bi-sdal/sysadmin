0436611ef21f8779b37e3c4214cbf311935fa527
Daniel Chen <chendaniely@users.noreply.github.com>
2017-08-07 15:13:30 -0400

    add -Rv to chmod proj_fdr

--- cron/setfacl_chmod_folders.sh
+++ cron/setfacl_chmod_folders.sh
@@ -1,7 +1,7 @@
 find /home/sdal/projects/ -mindepth 1 -maxdepth 1 -type d | while read proj_fdr
 do
 	echo $proj_fdr
-	chmod g+s $proj_fdr
+	chmod -Rv g+s $proj_fdr
 	setfacl -d -m g::rwx $proj_fdr
 	setfacl -d -m o::rx $proj_fdr
 	getfacl $proj_fdr
